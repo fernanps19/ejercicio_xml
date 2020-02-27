f80e4c6152d320093b3c8fbd6e8e8c8a67bb1bfc
Fernando Pérez Sánchez <fernanps19@gmail.com>
2020-02-27 13:09:31 +0100

    FuncionesXML.py

--- EjercicioXML.py
+++ EjercicioXML.py
@@ -7,9 +7,25 @@ from FuncionesXML import *
 #	print('nº',j,':',i)
 #	print(' -Descripción:',k)
 
-movimientos=contar(pokemon)
-ataques=[]
-for i in movimientos:
-	if i not in ataques:
-		ataques.append(i)
-print(len(ataques))
+#movimientos=contar(pokemon)
+#ataques=[]
+#for i in movimientos:
+#	if i not in ataques:
+#		ataques.append(i)
+#print(len(ataques))
+
+#tipo1=input('Introduzca un tipo:')
+#tipo2=input('Introduzca un segundo tipo (* para no buscar un segundo tipo):')
+#lista=buscar(pokemon,tipo1,tipo2)
+#for i in lista:
+#	print(i)
+
+#movimiento=input('Introduzca el movimiento a buscar:')
+#busqueda=buscar_relacionada(pokemon,movimiento)
+#print(movimiento,'pueden aprenderlo:')
+#for i in busqueda:
+#	print('nº',i[1],':',i[0])
+
+nombre=input('Introduzca el nombre del pokemon:')
+lista2=libre(pokemon,nombre)
+print(lista2)
--- FuncionesXML.py
+++ FuncionesXML.py
@@ -12,9 +12,34 @@ def contar(pokemon):
 
 #3. Buscar: Introducir por teclado uno o dos tipos y mostrar 
 #todos los pokemon que poseen esos tipos.
+def buscar(pokemon,tipo1,tipo2):
+	nombre=[]
+	lista=pokemon.xpath('/pokedex/pokemon')
+	for i in lista:
+		if tipo2 == '*':
+			if tipo1 in i.xpath('./type/text()'):
+				nombre.append(i.xpath('./name/text()')[0])
+		elif tipo1 in i.xpath('./type/text()') and tipo2 in i.xpath('./type/text()'):
+			nombre.append(i.xpath('./name/text()')[0])
+	return nombre
 
 
 #4. Buscar informacion relacionada: Introducir por teclado un movimiento y
 #mostrar los pokemon con su id que pueden aprender ese movimiento.
+def buscar_relacionada(pokemon,movimiento):
+	nombre=[]
+	ids=[]
+	lista=pokemon.xpath('/pokedex/pokemon')
+	for i in lista:
+		if movimiento in i.xpath('./moves/move/name/text()'):
+			nombre.append(i.xpath('./name/text()')[0])
+			ids.append(i.xpath('./@id')[0])
+	return zip(nombre,ids)
+
 #5. Libre: Introducir por teclado el nombre de un pokemon y 
 #mostrar el nombre de sus evoluciones, si tiene, con la id de cada uno y su descripción.
+def libre(pokemon,nombre):
+	lista=pokemon.xpath('/pokedex/pokemon')
+	for i in lista:
+		if nombre==i.xpath('./name/text()'):
+			 return i.xpath('./evolutions/evolution/name/text()')
